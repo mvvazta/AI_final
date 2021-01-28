26c3489c7df1842fd798304152c529fc1d04f5d6
MVV <mvv@atziestua.com>
2021-01-28 16:20:12 -0600

    lane lines recognition & drawing

--- README.md
+++ README.md
@@ -6,4 +6,14 @@
 
 3.-Se aplica un desenfoque Gaussiano para eliminar el ruido
 
-4.- La funcion canny aplica un desenfoque gaussiano e identifica los cambios de intesidad entre pixeles, lo cual ayuda a idenyidicar las lineas viales
+4.- La funcion canny aplica un desenfoque gaussiano e identifica los cambios de intesidad entre pixeles, lo cual ayuda a identidicar las lineas viales y reconstruye la imagen a partir de esos parámetros
+
+5.- con la libreria matplotlib generamos un plano en la imaen que nos ayudará a identificar más rápido las lineas viales
+
+6.- una vez que se identificó el carril que nos corresponde, se crea una máscara con el area que nos interesa
+
+7.- Con la ayuda del concepto bitwise (operaciones lógicas AND OR) vamos a comparar cada bit de la imagen entregada por la función canny y la máscara que hicimos en el paso anterior y con esto vamos a idicar cual va a ser el area de interés de la imagen
+
+8.- Con ayuda de la transformada de Hough podemos identificar en que punto de la imagen hay líneas, puntos o figuras y así poder discriminar los resultados que nosostros querramos y quedarnos sólo con las líneas viales
+
+9.- Una vez obtenidas la líneas, se combinan con la imagen original para comprobar que estas estén dibujadas correctamente
--- trainingImages/lane.py
+++ trainingImages/lane.py
@@ -1,10 +1,46 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt #5
+
+def cannyStep(image):
+		grayScale = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #2
+		blur = cv2.GaussianBlur(grayScale, (5, 5), 0) #3
+		canny = cv2.Canny(blur, 50, 150) #4
+		return canny
+
+def showLines(image, lines):
+	lineImg = np.zeros_like(image)
+	if lines is not None:
+		for line in lines:
+			x1, y1, x2, y2 = line.reshape(4)
+			cv2.line(lineImg, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+	return lineImg
+
+def regionMask(image): #6
+	height = image.shape[0]
+	#[(200, height), (1100, height), (550, 250)] for ll 4 and 5
+	regions = np.array([
+		[(310, height), (775, height), (775, 180), (442, 0), (366, 0)]
+		])
+	mask = np.zeros_like(image)
+	cv2.fillPoly(mask, regions, 255)
+	maskedImage = cv2.bitwise_and(canny, mask) #7
+	return maskedImage
 
 laneLines = cv2.imread('laneLines.jpg') #1
 laneBU = np.copy(laneLines)
-grayScale = cv2.cvtColor(laneBU, cv2.COLOR_RGB2GRAY) #2
-blur = cv2.GaussianBlur(grayScale, (5, 5), 0) #3
-canny = cv2.Canny(blur, 50, 150)
-cv2.imshow('result', canny)
+
+canny = cannyStep(laneBU)
+croppedImage = regionMask(canny)
+
+lines = cv2.HoughLinesP(croppedImage, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5) #8
+
+lineDrawed = showLines(laneBU, lines)
+
+mergedImage = cv2.addWeighted(laneBU, 0.8, lineDrawed, 1, 1)
+
+# plt.imshow(canny)
+# plt.show()
+cv2.imshow('region', mergedImage)
 cv2.waitKey(0)
